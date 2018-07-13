e54969fa8564df1ee3adb50a884378e988ea0834
kohei.shinohara <14kc1026@gmail.com>
2018-07-13 04:50:04 +0000

    edit func completed

--- resources/views/quizzes/edit.blade.php
+++ resources/views/quizzes/edit.blade.php
@@ -7,14 +7,14 @@
     {!! Form::model($quiz, ['route' => ['quizzes.update', $quiz->id], 'method' => 'put']) !!}
 
         {!! Form::label('title', 'TITLE:') !!}
-        {!! Form::text('title') !!}
+        {!! Form::text('title') !!}<br>
         
-        <!--{!! Form::label('question', 'QUESTION:') !!}-->
-        <!--{!! Form::text('question') !!}-->
+        {!! Form::label('question', 'QUESTION:') !!}
+        <input name="question" type="text" value = "{{$quiz->questions()->get()->toArray()[0]['question']}}" id="question"><br>
         
-        <!--{!! Form::label('answer', 'ANSWER:') !!}-->
-        <!--{!! Form::text('answer') !!}-->
-
+        {!! Form::label('answer', 'ANSWER:') !!}
+        <input name="answer" type="text" value = "{{$quiz->questions()->get()->toArray()[0]['answer']}}" id="answer"><br>
+        
         {!! Form::submit('UPDATE') !!}
 
     {!! Form::close() !!}
