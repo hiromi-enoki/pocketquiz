70139e37dd0b4ba4a943287bca345448046e7681
Yuki Miyasaka <yuki.m5911@gmail.com>
2018-07-13 01:55:07 +0000

    accordion page

--- resources/views/quizzes/show.blade.php
+++ resources/views/quizzes/show.blade.php
@@ -1,46 +1,38 @@
-<!--クイズ詳細ページ-->
-
 @extends('layouts.app')
 
 @section('content')
-            <!--確認用-->
-            <!--  <h2>quizzes.show</h2>-->
-    <div class="row">
-        <div class="image text-center">
-        <img src="{{ secure_asset("images/quizlist.jpg") }}" alt="quizlist pic"></div>
-        <div class="col-lg-12">
-            <div class="quiz">
-                <div class="panel panel-default">
-                    <div class="panel-heading text-center">
-                         <p class="question-title">QUIZ TITLE: {!! nl2br(e($quiz->title)) !!}</p>
-                    </div>
-                </div>
-                    <table class="table table-striped">
-                          <thead>
-                            <tr>
-                              <th scope="col">Question</th>
-                              <th scope="col">Answer</th>
-                            </tr>
-                          </thead>
-                          
-                          
-                          <tbody>
-                            
-                        
-                            <tr>
-                              <td>@foreach ($questions as $question)
-                            <a href="{{ route('quizzes.questions', $question->q_id) }}">{{ $question->question }}</a>
-                            @endforeach
-                                </td>
-                            
-                                 <td>@foreach ($answers as $answer)
-                            <a href="{{ route('quizzes.questions', $answer->q_id) }}">{{ $answer->answer }}</a>
-                            @endforeach
-                                </td>
-                            
-                            </tr>
-                            
-                    </table>
+<div class="text-center">
+    <img src="{{ secure_asset("images/quizlist.jpg") }}" alt="quizlist pic">
+
+<div class="row">
+<div class="col-lg-12">
+    <div class="quiz">
+    <div class="panel panel-default">
+    <div class="panel-heading text-center">
+        <p class="question-title">QUIZ TITLE:<br><br> {!! nl2br(e($quiz->title)) !!}</p>
+    </div>
+    
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<div class="panel-heading" role="tab" id="headingOne">
+        <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        @foreach ($quizsets as $quizset)
+        <p>Question:<br><br>▼click▼<br><br>{{ $quizset->question }}</p>
+        @endforeach
+        </a>
+        </h4>
+         </div>
+         <div id="collapseOne" class="panel-collapse collapse out" role="tabpanel"　aria-labelledby="headingOne">
+         <div class="panel-body">
+         @foreach ($quizsets as $quizset)
+         <p>Answer:<br><br> {{ $quizset->answer }}</p>
+         @endforeach
+         </div>
+         </div>
+         </div>
+        </div>
+        </div>
+            
                     
                     <!--<div class="panel-body">-->
                        <!--trying to access questions.blade-->
@@ -49,11 +41,5 @@
                     <!--        <a href="{{ route('quizzes.questions', $question->q_id) }}">{{ $question->question }}</a>-->
                     <!--    @endforeach-->
                       
-                    </div>
-                    
-                    </div>
-                
-              
-            </div>
+                  
     @endsection
-        </div>
