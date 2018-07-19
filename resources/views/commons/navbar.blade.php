a685be58155ad6867960f4ab60b4cc3ba3ae2014
minako.yanagi <mnk14s@yahoo.co.jp>
2018-07-19 05:58:31 +0000

    added small changes

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -29,28 +29,12 @@
 	                </div>
 			             <button type="submit" class="btn btn-default">GO</button>
 		             </form>
-                        <!--<li>{!! link_to_route('quizzes.create', 'Make a NEW QUIZ', auth()->user()->id) !!}</li>クリエイトボタンデザイン変更-->
-                    <li>
-                         <a href="{{ route('quizzes.create', auth()->user()->id) }}" class="makeanewquiz_btn">Make a NEW QUIZ</a>
-                    </li>
-                        <!--クリエイトボタン -->
-                    <li>  
-                        
-                        <div class="cp_cont">
-                        <div class="cp_offcm01">
-                        <input type="checkbox" id="cp_toggle01">
-                        <label for="cp_toggle01"><span></span></label>
-                        <div class="cp_menu">
-            <ul>
-                <li>{!! link_to_route('users.mypage', 'My profile', ['id' => Auth::id()]) !!}</li>
-                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
-            </ul>
-            
+                 
                         <!--<li>{!! link_to_route('quizzes.create', 'Make a NEW QUIZ', auth()->user()->id) !!}</li>旧クリエイトボタン-->
                         <li>{!! link_to_route('quizzes.create', 'Make a NEW QUIZ', ['id' => Auth::id()], ['class' => 'blue_btn']) !!}</li>
                         <!--クリエイトボタン -->
 
-                            <li class="visible-xs">{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()], ['class' => 'blue_btn']) !!}</li>
+                            <li class="visible-xs">{!! link_to_route('users.mypage', 'My profile', ['id' => Auth::id()], ['class' => 'blue_btn']) !!}</li>
                             <li class="visible-xs">{!! link_to_route('logout.get', 'Logout', null, ['class' => 'blue_btn']) !!}</li>
 
                         <li class="hidden-xs">
@@ -60,7 +44,7 @@
                             <label for="cp_toggle01"></label>
                                 <div class="cp_menu">
                                     <ul>
-                                        <li>{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
+                                        <li>{!! link_to_route('users.mypage', 'My profile', ['id' => Auth::id()]) !!}</li>
                                         <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
                                     </ul>
                                 </div>
