5ca78cd6dc2957b36b93e51911168c5a1f0488ed
kohei.shinohara <14kc1026@gmail.com>
2018-07-19 03:06:28 +0000

    two edit functions conbined

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -207,10 +207,14 @@ public function show($id)
         public function edit($id)
     {
        $quiz = \App\Quiz::find($id);
-    //   $question = \DB::table('quizzes')->join('questions', 'quizzes.id', '=', 'questions.q_id')->select('questions.question','questions.answer')->get();
+      $question = \DB::table('quizzes')->join('questions', 'quizzes.id', '=', 'questions.q_id')->select('questions.question','questions.answer')->get();
+              $questions = $quiz->questions()->orderBy('created_at', 'desc')->paginate(10);
+
 
         return view('quizzes.edit', [
             'quiz' => $quiz,
+            'question' => $question,
+            'questions' => $questions
         ]);
     }
     
