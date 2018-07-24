078a33b758a77428f94e56f8f417bd58793564eb
hiromi.enoki <prismblaqars@gmail.com>
2018-07-24 03:34:42 +0000

    quizzes blade icon color changed

--- resources/views/quizzes/show.blade.php
+++ resources/views/quizzes/show.blade.php
@@ -19,14 +19,15 @@
                     <div class="panel-heading" role="tab" id="headingOne">
                         <h4 class="panel-title">
                             <a role="button" data-toggle="collapse" data-parent="#accordion{{$i}}" href="#collapseOne{{$i}}" aria-expanded="true" aria-controls="collapseOne">
-                                <span class="glyphicon glyphicon-question-sign text-info"></span>{!! nl2br(e($question->question)) !!}
-                                <span class="glyphicon glyphicon-chevron-down pull-right"></span>
+                                <span class="glyphicon glyphicon-question-sign text-info" style="color:#f9e75e">
+                                </span> {!! nl2br(e($question->question)) !!}
+                                <span class="glyphicon glyphicon-chevron-down pull-right" ></span>
                             </a>
                         </h4>
                     </div>
                     <div id="collapseOne{{$i}}" class="panel-collapse collapse out" role="tabpanel"aria-labelledby="headingOne">
                         <div class="panel-body">
-                            <span class="glyphicon glyphicon-pencil text-info"></span>{!! nl2br(e($question->answer)) !!}
+                            <span class="glyphicon glyphicon-pencil text-info" style="color:#0d7e83"></span> {!! nl2br(e($question->answer)) !!}
                         </div>
                     </div>
         </div>
